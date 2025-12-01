12896b63b8c6b465afa168c2fdf1a46a924b16f2
Shafiil Ahmed Sadat <sadatshafiil@gmail.com>
2025-12-01 10:28:16 +0600

    Create CF-A-Calculating-Function.cpp

---
+++ CF-A-Calculating-Function.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     long long n,x;
+     cin >> n ;
+     if(n%2==0){
+          x = n/2 ;
+     }
+     else
+     {
+          x = -((n+1)/2) ;
+     }
+     cout << x << endl;
+      return 0;
+}
